42999dfb5731d3c38bb3dff37530d651d2e09927
Jordan Bach <jordanbach@gmail.com>
2020-07-31 08:56:12 -0500

    refactoring the parser into something extensible

--- test/note_parser_test.js
+++ test/note_parser_test.js
@@ -2,50 +2,89 @@ import { strict as assert } from 'assert'
 import { NoteParser } from '../lib/note_parser.js'
 
 export class NoteParserTest {
+  parseNote(text, debug = false) {
+    let parser = new NoteParser(text, debug)
+    parser.parse()
+    return parser
+  }
+
   testEmpty() {
-    const text = ''
-    let tree = NoteParser.parse(text)
-    assert.deepStrictEqual(tree, [':DOC'])
+    let parser = this.parseNote('')
+    assert.deepStrictEqual(parser.tree.toArray(), [':DOC'])
   }
 
   testSentence() {
-    const text = 'This is a simple sentence.'
-    let tree = NoteParser.parse(text)
-    assert.deepStrictEqual(tree,
+    let parser = this.parseNote('This is a simple sentence.')
+    assert.deepStrictEqual(parser.tree.toArray(),
       [':DOC',
-        [':P', 'This is a simple sentence.']])
+        [':BLOCK',
+          [':TEXT', 'This is a simple sentence.']]])
   }
 
   testParagraphs() {
-    const text =
+    let parser = this.parseNote(
 `This is the first paragraph.
 
-This is the second paragraph.`
-    let tree = NoteParser.parse(text)
-    assert.deepStrictEqual(tree,
+This is the second paragraph.`)
+    assert.deepStrictEqual(parser.tree.toArray(),
       [':DOC',
-        [':P', 'This is the first paragraph.'],
-        [':P', 'This is the second paragraph.']])
+        [':BLOCK',
+          [':TEXT', 'This is the first paragraph.']],
+        [':BLOCK',
+          [':TEXT', 'This is the second paragraph.']]])
   }
 
   testMultilineParagraph() {
-    const text =
+    let parser = this.parseNote(
 `
 This is a paragraph that
 spans multiple lines.
+`)
+    assert.deepStrictEqual(parser.tree.toArray(),
+      [':DOC',
+        [':BLOCK',
+          [':TEXT', 'This is a paragraph that spans multiple lines.']]])
+  }
+
+  testH1() {
+    let parser = this.parseNote('# this is a heading')
+    assert.deepStrictEqual(parser.tree.toArray(),
+      [':DOC',
+        [':H1',
+          [':TEXT', 'this is a heading']]])
+  }
+
+  testUL() {
+    let parser = this.parseNote(
+`
+* backpack
+* sleeping bag
+* socks
+* head lamp
+`)
+    assert.deepStrictEqual(parser.tree.toArray(),
+      [':DOC',
+        [':UL',
+          [':LI', [':TEXT', 'backpack']],
+          [':LI', [':TEXT', 'sleeping bag']],
+          [':LI', [':TEXT', 'socks']],
+          [':LI', [':TEXT', 'head lamp']]]])
+  }
+
+  testHyperlink() {
+    return 'skip'
+    const text =
+`
+This code is [hosted on GitHub](https://github.com/jbgo/pkr).
 `
     let tree = NoteParser.parse(text)
     assert.deepStrictEqual(tree,
       [':DOC',
-        [':P', 'This is a paragraph that spans multiple lines.']])
-  }
-
-  testH1() {
-    const text = '# this is a heading'
-    let tree = NoteParser.parse(text)
-    assert.deepStrictEqual(tree,
-      [':DOC',
-        [':H1',
-          [':TEXT', 'this is a heading']]])
+        [':P',
+          [':TEXT', 'This code is'],
+          [':LINK',
+            {'url': 'https://github.com/jbgo/pkr'},
+            [':TEXT', 'hosted on GitHub']],
+          [':TEXT', '.']]])
   }
 }
